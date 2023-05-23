bedd4aebb9d28e53b1514b9ec9a47f749923d314
rohan kumar majhi <rohandevil6@gmail.com>
2023-05-24 04:41:48 +0530

    added badge for bottom nav

--- app/src/main/java/com/example/aislepoc/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/aislepoc/ui/main/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.aislepoc.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.aislepoc.R
 import com.example.aislepoc.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity: AppCompatActivity() {
@@ -19,7 +25,7 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setBadge()
     }
 
     fun showBottomNav(visible: Boolean){
@@ -28,6 +34,22 @@ class MainActivity: AppCompatActivity() {
         } else {
             View.GONE
         }
+    }
+
+    fun setBadge(){
+        val matchBadge = binding.bottomNavigation.getOrCreateBadge(R.id.matches)
+        matchBadge.let {
+            it.isVisible = true
+            it.number = 50
+            it.backgroundColor = resources.getColor(R.color.purple)
+        }
+        val noteBadge = binding.bottomNavigation.getOrCreateBadge(R.id.notes)
+        noteBadge.let {
+            it.isVisible = true
+            it.number = 9
+            it.backgroundColor = resources.getColor(R.color.purple)
+        }
+
     }
 
     override fun onDestroy() {
