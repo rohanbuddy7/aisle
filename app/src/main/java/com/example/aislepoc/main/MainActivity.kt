adf10602d2ded41818be9e3c704b48d864f5018e
rohan kumar majhi <rohandevil6@gmail.com>
2023-05-23 09:47:02 +0530

    project setup

--- app/src/main/java/com/example/aislepoc/main/MainActivity.kt
+++ app/src/main/java/com/example/aislepoc/main/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.example.aislepoc
+package com.example.aislepoc.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.aislepoc.R
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
     }
 
 }
