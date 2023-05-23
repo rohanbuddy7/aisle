b2d62bbeba62e817d957c365038982b5bfe0a312
rohan kumar majhi <rohandevil6@gmail.com>
2023-05-23 23:12:56 +0530

    otp screen

---
+++ app/src/main/java/com/example/aislepoc/otp/OtpFragment.kt
@@ -0,0 +1,98 @@
+package com.example.aislepoc.otp
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.aislepoc.R
+import com.example.aislepoc.databinding.FragmentOtpBinding
+import java.util.Locale
+
+
+class OtpFragment : Fragment() {
+    private var countDownTimer: CountDownTimer? = null
+    private var phone: String? = null
+    private var _binding: FragmentOtpBinding? = null
+    private val binding: FragmentOtpBinding
+        get() {
+            return _binding!!
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            phone = it.getString("phone")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOtpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+        setupListeners()
+        startTimer()
+    }
+
+    private fun setup() {
+        phone?.let {
+            binding.textPhone.text = it
+        }
+    }
+
+
+    private fun setupListeners() {
+        binding.textPhone.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.bttnContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_otpFragment_to_notesFragment)
+        }
+    }
+
+    fun startTimer(){
+        val duration = 60000L
+        countDownTimer = object : CountDownTimer(duration, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                var seconds = millisUntilFinished / 1000
+                val minutes = seconds / 60
+                seconds %= 60
+                val time = String.format(
+                    Locale.getDefault(),
+                    "%02d:%02d",
+                    minutes,
+                    seconds
+                )
+                binding.textTimer.text = time
+            }
+            override fun onFinish() {}
+        }
+        countDownTimer?.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        countDownTimer?.let {
+            it.cancel()
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = OtpFragment()
+    }
+}
--- app/src/main/java/com/example/aislepoc/phoneNumber/PhoneNumberFragment.kt
+++ app/src/main/java/com/example/aislepoc/phoneNumber/PhoneNumberFragment.kt
@@ -5,39 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.findNavController
 import com.example.aislepoc.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.aislepoc.databinding.FragmentPhoneBinding
 
 class PhoneNumberFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentPhoneBinding? = null
+    private val binding: FragmentPhoneBinding
+        get() {
+            return _binding!!
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_phone, container, false)
+    ): View {
+        _binding = FragmentPhoneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.bttnContinue.setOnClickListener {
+            proceed()
+        }
+    }
+
+    fun proceed(){
+        //binding.etCode.text.toString() + " " + binding.etPhone.text.toString()
+        val phone = String.format("%s %s",binding.etCode.text.toString(), binding.etPhone.text.toString() )
+        val bundle = Bundle();
+        bundle.putString("phone", phone)
+        findNavController().navigate(R.id.action_phoneFragment_to_OTPFragment, bundle)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PhoneNumberFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(param1: String, param2: String) = PhoneNumberFragment()
     }
+
 }
